beebd3531a8e70d6ed8c94cbd04aecb736511e15
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-04-18 00:52:36 +0900

    Player Auto Move

--- Smile/Assets/Scripts/Play/PlayerMove.cs
+++ Smile/Assets/Scripts/Play/PlayerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private int moveSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + (transform.right * moveSpeed * Time.deltaTime);
     }
+
 }
