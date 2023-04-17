b58eff15c25e97f9591ee6582d13ac5439f32b65
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-04-18 02:38:01 +0900

    Show Note & Note Touch

--- Smile/Assets/Scripts/Play/NoteTouch.cs
+++ Smile/Assets/Scripts/Play/NoteTouch.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class NoteTouch : MonoBehaviour
 {
-    [SerializeField] private int moveSpeed;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +13,11 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (transform.right * moveSpeed * Time.deltaTime);
+        
     }
 
+    public void touchClick()
+    {
+        Debug.Log("touch" + this.name);
+    }
 }
