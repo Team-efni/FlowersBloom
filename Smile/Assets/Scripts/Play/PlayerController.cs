c6851becf1bd2d1060b31549e7f5d54f315c838e
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-04-28 15:40:11 +0900

    몬스터 실패시 기회 포인트 1차감

--- Smile/Assets/Scripts/Play/PlayerController.cs
+++ Smile/Assets/Scripts/Play/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int moveSpeed;
 
     public IScenePass scenePass;
+
+    // 기회 포인트 관련
+    public int notePoint = 2; // 기회 포인트
+    public GameObject[] go_notePoints; // 기회 포인트 오브젝트
+
+    // 목숨 포인트 관련
+    public int lifePoint = 3; // 목숨 포인트
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,14 +34,39 @@ public class PlayerController : MonoBehaviour
         if(collision.CompareTag("Monster"))
         {
             // 몬스터에 닿으면 멈추고 RN 씬 호출
-            moveSpeed = 0;
             Debug.Log("Go to RN");
+            
+            moveSpeed = 0;
 
+            // 기회가 남아있다면 감소하고 씬 이동
+            if (notePoint > 0)
+            {
+                notePoint--;
+                go_notePoints[notePoint].gameObject.SetActive(false);
+
+                //씬 바로 이동
+                scenePass.SceneLoadStart();
+            }
+
+            // 기회가 0이라면 목숨 포인트 감소
+            else if (notePoint == 0)
+            {
+                // 목숨 포인트가 남아있다면 감소
+                if(lifePoint > 0)
+                {
+                    lifePoint--;
+                }
+                // 남아있지 않다면 게임 오버    
+                else if(lifePoint == 0)
+                {
+                    Debug.Log("GameOver");
+                }
+            }
+                
 
             //애니메이션 주기
 
-            //씬 바로 이동
-            scenePass.SceneLoadStart();
+            
         }
     }
 }
