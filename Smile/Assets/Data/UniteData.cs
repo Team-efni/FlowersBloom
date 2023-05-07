0672049d76eaa55fdcb0bb0102201f28b6f36a19
TheHyperPay <gkfldys1276@yandex.com>
2023-05-07 18:14:42 +0900

    Play에서 둠칫거리던 튤립 애니메이션 버그 수정

--- Smile/Assets/Data/UniteData.cs
+++ Smile/Assets/Data/UniteData.cs
@@ -15,7 +15,7 @@ public class UniteData
     public static Vector2 Player_Location_Past = new Vector2(-4955f, 0); //플레이어가 이전에 있던 위치
     public static float Play_Scene_Time = 0f; //플레이 씬에서의 흐른 시건
 
-    public static string Selected_Character = "Dandelion"; //유저가 선택한 캐릭터 ["Dandelion", "Tulip"]
+    public static string Selected_Character = "Tulip"; //유저가 선택한 캐릭터 ["Dandelion", "Tulip"]
     public static string Closed_Monster = "Rose"; //최근접 몬스터 ["Rose", "Cosmos", "MorningGlory"]
 
     public static bool ReStart = true; // 유저가 재시작 혹은 나가기 버튼을 누른 경우
@@ -24,7 +24,7 @@ public class UniteData
     public static bool[] GameClear = { false, false, false}; // 난이도별 클리어 유무
 
     //설정 관련 데이터
-    public static float BGM = 1f; //배경음악 볼륨
+    public static float BGM = 0f; //배경음악 볼륨
     public static float Effect = 1f; //효과음 볼륨
 
     //게임 파일 관련 데이터
