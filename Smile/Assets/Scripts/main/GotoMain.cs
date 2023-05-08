24ba6927bec69e5f1eddfd4ba0a1490827fda454
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-05-09 03:24:16 +0900

    캐릭터 선택창 뒤로가기 작동안되는 버그 수정

--- Smile/Assets/Scripts/main/GotoMain.cs
+++ Smile/Assets/Scripts/main/GotoMain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MapBackBtn : MonoBehaviour
+public class GotoMain : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
