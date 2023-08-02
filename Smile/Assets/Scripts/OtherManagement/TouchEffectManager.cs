2279df6547bfce3614cd7df50134b0180e6f65b4
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-08-02 20:59:41 +0900

    터치 이펙트 가능한 씬 추가
    - IN Game-RN, 캐릭터 씬 추후 수정 필요

--- Smile/Assets/Scripts/OtherManagement/TouchEffectManager.cs
+++ Smile/Assets/Scripts/OtherManagement/TouchEffectManager.cs
@@ -1,24 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class TouchEffectManager : MonoBehaviour
 {
     public ParticleSystem particlePrefab;
+    private string canvasName = "Canvas";
+    private Canvas canvas;
+
+    void Start()
+    {
+        FindCanvas();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     void Update()
     {
+        if (canvas == null) FindCanvas();
         if (Input.GetMouseButtonUp(0))
         {
-            Vector2 touchPosition = Input.mousePosition;
-            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            Vector2 touchPosition = GetTouchPosition();
             ParticleSystem newParticle = Instantiate(particlePrefab, touchPosition, Quaternion.identity);
             newParticle.Play();
             Destroy(newParticle.gameObject, 1.0f);
         }
     }
 
-    private void Awake()
+    void FindCanvas()
     {
-        DontDestroyOnLoad(this);
+        Debug.Log("find");
+        canvas = GameObject.Find(canvasName).GetComponent<Canvas>();
+    }
+
+    Vector2 GetTouchPosition()
+    {
+        Vector2 touchPosition = Input.mousePosition;
+        switch (canvas.renderMode)
+        {
+            case RenderMode.ScreenSpaceOverlay:
+                break;
+            case RenderMode.ScreenSpaceCamera:
+                touchPosition = canvas.worldCamera.ScreenToWorldPoint(touchPosition);
+                break;
+            case RenderMode.WorldSpace:
+                touchPosition = canvas.transform.InverseTransformPoint(touchPosition);
+                break;
+        }
+        if(SceneManager.GetActiveScene().name == "Play")
+        {
+            Debug.Log("play");
+            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+        }
+        Debug.Log(touchPosition);
+        return touchPosition;
     }
 }
