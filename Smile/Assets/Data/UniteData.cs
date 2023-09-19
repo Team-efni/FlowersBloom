dc216b73a7030df293fc8b728eab3066253f61c7
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-09-20 02:01:58 +0900

    🎈 UniteData 수정사항 롤백

--- Smile/Assets/Data/UniteData.cs
+++ Smile/Assets/Data/UniteData.cs
@@ -18,7 +18,7 @@ public class UniteData
     public static Vector2 Player_Location_Past = new Vector2(-4955f, 0); //플레이어가 이전에 있던 위치
     public static float Play_Scene_Time = 0f; //플레이 씬에서의 흐른 시간
 
-    public static string Selected_Character = "ForgetMeNot"; //유저가 선택한 캐릭터 ["Dandelion", "Tulip", "ForgetMeNot"]
+    public static string Selected_Character = PlayerPrefs.GetString("Selected_Character", "Dandelion"); //유저가 선택한 캐릭터 ["Dandelion", "Tulip", "ForgetMeNot"]
     public static string Closed_Monster = "Rose"; //최근접 몬스터 ["Rose", "Cosmos", "MorningGlory", "Poppy", "Pasque"]
 
     public static bool ReStart = true; // 유저가 재시작 혹은 나가기 버튼을 누른 경우
